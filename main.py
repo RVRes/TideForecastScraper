d206030ab0b06abfc34ecce763c782a78244fee7
RomanReshetnikov <rv.reshetnikov@gmail.com>
2021-06-11 23:11:41 -0500

    Result output table changed to more accurate.

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ if __name__ == '__main__':
     scraper = TideForecastScraper()
     result = scraper.get_all_daylights_low_tides
     for place, tides in result.items():
-        print('', place, sep='\n')
-        print(tabulate(tides, tablefmt="grid"))
+        print('', place, '-' * 30, sep='\n')
+        print(tabulate(tides, headers="keys", tablefmt="plain"))
